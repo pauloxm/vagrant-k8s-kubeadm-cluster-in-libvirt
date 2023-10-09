77a8a502a27ea77fc0fe6186bb00228d9d6b0ada
Paulo Xavier <pauloxmachado@gmail.com>
2023-10-09 14:03:29 -0300

    Corrige script

--- cluster-kubeadm/provision_linuxtips.sh
+++ cluster-kubeadm/provision_linuxtips.sh
@@ -30,7 +30,7 @@ sudo apt-get update
 
 # Instalando Containerd
 
-sudo apt update && sudo apt install -y containerd.io -y
+sudo apt update && sudo apt install -y containerd -y
 
 # Configurar containerd gerando arquivo de configuração
 
@@ -43,16 +43,16 @@ systemctl restart containerd
 ## Instalação do kubeadm, kubelet and kubectl
 
 sudo apt-get update && \
-sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
-curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-apt-keyring.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Instalar versão específica do Kubernetes
 
 sudo apt-get update && \
-sudo apt-get install -y kubelet=1.23.6-00 kubeadm=1.23.6-00 kubectl=1.23.6-00
+sudo apt-get install -y kubelet kubeadm kubectl
 
 # Configurar CRI
 
